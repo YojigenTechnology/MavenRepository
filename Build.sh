4257ae633da39d989b21e0012268ca5e33b26f1f
某亚瑟 <i@yojigen.tech>
2020-07-09 13:25:15 +0800

    Update

--- Build.sh
+++ Build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-echo '克隆仓库'
 git clone https://github.com/mouyase/YojigenUtils.git
 cd YojigenUtils
 chmod +x gradlew
-echo '编译代码'
 ./gradlew uploadArchives
 cd ..
-echo '复制文件'
-cp -rf YojigenUtils/MavenRepository/ ./
+cp -rf YojigenUtils/MavenRepository .
+rm -rf YojigenUtils
